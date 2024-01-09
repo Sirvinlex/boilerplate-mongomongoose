056d31680358f14ce102c7f2cfc79ce35514c9b2
Alex <vinlex3030@gmail.com>
2024-01-09 18:17:09 +0100

    updated task

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
  
-const testing = 'testing'
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
