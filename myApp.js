6924b4927f48585bb57bd01dbf4f036ca185dd0e
Alex <vinlex3030@gmail.com>
2024-01-10 16:22:09 +0100

    updated task

--- myApp.js
+++ myApp.js
@@ -100,10 +100,14 @@ const queryChain = (done) => {
   .sort({ name: -1 })
   .limit(2)
   .select({ age: 0 })
-  .exec(function(err, data) {
-    if(err) return console.log(err);
-    done(null, data);
-  });
+  .exec.then(function(err, docs) {
+  if(err) return console.log(err);
+    done(null, docs);
+});
+  // .exec(function(err, data) {
+    // if(err) return console.log(err);
+    // done(null, data);
+  // });
   // done(null /*, data*/);
 };
 
